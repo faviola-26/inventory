9fd6ca9f0945ba164db66c4c64230355f2db9795
david.martinez <david.martinez@SAO-LT-191014B.ad.unosquare.com>
2020-05-30 13:39:50 -0500

    Unfinished ActiveProduct entity

---
+++ src/main/java/com/mycompany/inventory/model/ActiveProduct.java
@@ -0,0 +1,11 @@
+package com.mycompany.inventory.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+//@Entity
+//@Table(name = "active_product", catalog = "inventory")
+public class ActiveProduct implements Serializable{
+    
+}
